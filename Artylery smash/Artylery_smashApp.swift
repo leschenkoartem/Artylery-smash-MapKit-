df3e9b50280ca7e860694e270a372add45ad9b27
leschenkoartem <leschenko.artem@lll.kpi.ua>
2023-11-16 15:42:20 +0200

    Initial commit

---
+++ Artylery smash/Artylery_smashApp.swift
@@ -0,0 +1,17 @@
+//
+//  Artylery_smashApp.swift
+//  Artylery smash
+//
+//  Created by Artem Leschenko on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Artylery_smashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
